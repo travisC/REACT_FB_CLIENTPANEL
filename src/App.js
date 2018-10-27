ba540c59e9efec35786a587a4c411a1d49a65414
Travis Cunningham <travis@enviral.io>
2018-10-27 09:50:15 -0700

    Add login form and init FB auth

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import { Provider } from 'react-redux';
-import store from './store';
+import { Provider } from "react-redux";
+import store from "./store";
 
-import AppNavbar from './components/layout/AppNavbar';
-import Dashboard from './components/layout/Dashboard';
-import AddClient from './components/clients/AddClient';
-import ClientDetails from './components/clients/ClientDetails';
-import EditClient from './components/clients/EditClient';
+import AppNavbar from "./components/layout/AppNavbar";
+import Dashboard from "./components/layout/Dashboard";
+import AddClient from "./components/clients/AddClient";
+import ClientDetails from "./components/clients/ClientDetails";
+import EditClient from "./components/clients/EditClient";
+import Login from "./components/auth/Login";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router> 
+        <Router>
           <div className="App">
             <AppNavbar />
             <div className="container">
               <Switch>
-                <Route exact path="/" component={ Dashboard } />
-                <Route exact path="/client/add" component={ AddClient } />
-                <Route exact path="/client/:id" component={ ClientDetails } />
-                <Route exact path="/client/edit/:id" component={ EditClient } />
+                <Route exact path="/" component={Dashboard} />
+                <Route exact path="/client/add" component={AddClient} />
+                <Route exact path="/client/:id" component={ClientDetails} />
+                <Route exact path="/client/edit/:id" component={EditClient} />
+                <Route exact path="/login" component={Login} />
               </Switch>
             </div>
           </div>
