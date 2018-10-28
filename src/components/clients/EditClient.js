be70149df57679a1a59e8d02a753423396ccdb6a
Travis Cunningham <travis@enviral.io>
2018-10-27 17:00:36 -0700

    Implement setting results

--- src/components/clients/EditClient.js
+++ src/components/clients/EditClient.js
@@ -42,7 +42,7 @@ class EditClient extends Component {
 
   render() {
     const { client } = this.props;
-
+    const { disableBalanceOnEdit } = this.props.settings;
     if (client) {
       return (
         <div>
@@ -111,6 +111,7 @@ class EditClient extends Component {
                     name="balance"
                     ref={this.balanceInput}
                     defaultValue={client.balance}
+                    disabled={disableBalanceOnEdit}
                   />
                 </div>
 
@@ -138,7 +139,8 @@ export default compose(
   firestoreConnect(props => [
     { collection: "clients", storeAs: "client", doc: props.match.params.id }
   ]),
-  connect(({ firestore: { ordered } }, props) => ({
-    client: ordered.client && ordered.client[0]
+  connect(({ firestore: { ordered }, settings }, props) => ({
+    client: ordered.client && ordered.client[0],
+    settings: settings
   }))
 )(EditClient);
